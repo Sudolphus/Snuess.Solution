dd0fd674fbd536f305b3ed9647dd076a26d5f6ee
Micheal Hansen <micheal.hansen@gmail.com>
2020-08-07 10:17:28 -0700

    add Index method to engineer controller

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      IQueryable<Engineer> engineerQuery = _db.Engineers;
+      IEnumerable<Engineer> engineerList = engineerQuery
+        .ToList()
+        .OrderBy(eng => eng.LastName)
+        .ThenBy(eng => eng.FirstName);
+      return View(engineerList);
+    }
+  }
+}
